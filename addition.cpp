0958728f4d206cca6320d7c2f036e812bf6ccde3
SahanPasindu <pasindusahan62@example.com>
2025-03-06 15:14:28 +0530

    add two num and display summ

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, sum;
+    char operation;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    sum=num1+num2;
+    cout <<  sum ;
+    
+}
